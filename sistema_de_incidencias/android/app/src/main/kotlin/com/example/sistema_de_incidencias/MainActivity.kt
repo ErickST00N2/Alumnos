7cffb0a133cdd48db9e390b8d61179ae851dc85b
ErickST00N <78329412+ErickST00N@users.noreply.github.com>
2024-03-28 02:14:14 -0600

    Delete sistema_de_incidencias directory

--- sistema_de_incidencias/android/app/src/main/kotlin/com/example/sistema_de_incidencias/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sistema_de_incidencias
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- sistema_de_incidencias/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:sistema_de_incidencias/src/app.dart';
-
-void main() => runApp(const MyApp());
